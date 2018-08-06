810261e828ef92c8b191c69576d5b5eac8812ef9
antoniocaiazzo <antonio.caiazzo87@hotmail.it>
2018-08-06 17:48:47 +0200

    Forward propagation

--- ForwardPropagation.py
+++ ForwardPropagation.py
@@ -2,3 +2,11 @@ import numpy as np
 import Net
 
 class ForwardPropagation:
+    def feed_forward (self, X):
+        z={}
+
+        a={1:x}
+        for i in range(1, net.n_layers):
+            z[1+1]=np.dot(a[i], self.w[i])+self.b[i]
+            a[i]+1=net.activations[i+1](3)
+        return a[net.n_layers]
