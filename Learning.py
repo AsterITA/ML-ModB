b2399a9813d79e6b850048155fbac714ca65eb62
antim <antimodebiase@hotmail.it>
2018-08-07 14:56:11 +0200

    Ho corretto alcuni errori e sto preparando un test per testare il learning. Rimane in sospeso la questione delle funzioni di errore inseribili dall'utente.

--- Learning.py
+++ Learning.py
@@ -14,23 +14,21 @@ class MultiPerceptronLearning:
         for t in range(self.max_epoch):
             #permutazione del training set
             np.random.shuffle(training_set)
-            for n in range(training_set.__sizeof__()):
-                ########################
-                # PROBABILMENTE DA MODIFICARE
-                outputs = net.forward_propagation_with_output_hidden(training_set[n].input)
-                deltas = self.back_propagation(net, training_set[n].labels, outputs)
-                derivatives = self.calc_derivatives(net, training_set[n].input, outputs, deltas)
+            for n in range(len(training_set)):
+                act_and_out = net.forward_propagation_with_output_hidden(training_set[n]['input'])
+                deltas = self.back_propagation(net, training_set[n]['label'], act_and_out['outputs'], act_and_out['activations'])
+                derivatives = self.calc_derivatives(net, training_set[n]['input'], act_and_out['outputs'], deltas)
                 self.update_weights(net, derivatives)
             #Calcolo dell'errore sul training set
             error_training = 0
-            for n in range(training_set.__sizeof__()):
-                output = net.forward_propagation(training_set[n].input)
-                error_training += self.error_function #questo di sicuro è da modificare
+            for n in range(len(training_set)):
+                output = net.forward_propagation(training_set[n]['input'])
+                error_training += self.error_function(output, training_set[n]['label'])
             #Calcolo dell'errore sul validation set
             error_validation = 0
-            for n in range(validation_set.__sizeof__()):
-                output = net.forward_propagation(validation_set[n].input)
-                error_validation += self.error_function  # questo di sicuro è da modificare
+            for n in range(len(validation_set)):
+                output = net.forward_propagation(validation_set[n]['input'])
+                error_validation += self.error_function(output, validation_set[n]['label'])
             #La rete di questo passo è migliore?
             try:
                 if best_error_validation > error_validation:
@@ -45,25 +43,23 @@ class MultiPerceptronLearning:
                 break
         return best_net
 
-    def back_propagation(self, net, labels, outputs):
+    def back_propagation(self, net, labels, outputs, node_act):
         #Calcolo delta
-        delta_out = net.primes[net.n_layers - 1](net.activations[net.n_layers - 1]) * (outputs[net.n_layers - 1] - labels)
         deltas = np.ndarray(net.n_layers, dtype=np.ndarray)
-        deltas[net.n_layers - 1] = delta_out
+        deltas[net.n_layers - 1] = net.primes[net.n_layers - 1](node_act[net.n_layers - 1]) * (outputs[net.n_layers - 1] - labels)
         for l in range(net.n_layers - 2, -1, -1):
             deltas[l] = np.dot(deltas[l + 1], net.W[l+1])
-            deltas[l] = net.primes[l](net.activations[l]) * deltas[l]#da modificare
+            deltas[l] = net.primes[l](node_act[l]) * deltas[l]
         return deltas
 
     def calc_derivatives(self, net, input, outputs, deltas):
         #Calcolo derivate
-        derivate_W = np.ndarray(net.livelli, dtype=np.ndarray)
-        derivate_B = np.ndarray(net.livelli, dtype=np.ndarray)
+        derivate_W = []
+        derivate_B = []
         Z = input
-        Z = Z.reshape(1, len(Z))
-        for l in range(0, net.n_layers):
-            derivate_W[l] = np.dot(deltas[l].T, Z)
-            derivate_B[l] = deltas[l]
+        for l in range(net.n_layers):
+            derivate_W.append(np.dot(deltas[l][:,np.newaxis]), Z[np.newaxis,:])
+            derivate_B.append(deltas[l])
             Z = outputs[l]
         derivatives = {'weights': derivate_W, 'bias': derivate_B}
         return derivatives
@@ -71,5 +67,29 @@ class MultiPerceptronLearning:
     def update_weights(self, net, derivatives):
         #Aggiornamento dei pesi
         for l in range(net.n_layers - 1):
-            self.W[l] = self.W[l] - self.eta * derivatives['weights'][l]
-            self.B[l] = self.B[l] - self.eta * derivatives['bias'][l]
+            net.W[l] = net.W[l] - self.eta * derivatives['weights'][l]
+            net.B[l] = net.B[l] - self.eta * derivatives['bias'][l]
+
+
+
+#LE RIGHE SUCCESSIVE MI SERVIRANNO PER FARE UN TEST
+import Net
+def sum_square(t,y):
+    err=0
+    for i in range(y.size):
+        err+=(y[i]-t[i])**2
+        err/=2
+    return err
+
+#Genero un training set
+training_set = []
+for i in range(10):
+    inpu = np.random.randint(0,4,4)
+    app = np.random.randint(0,2,1)
+    out = np.zeros(3)
+    out[app] = 1
+    elem = {'input':inpu, 'label': out}
+    training_set.append(elem)
+print(training_set)
+#Creazione rete
+#Addestramento rete
