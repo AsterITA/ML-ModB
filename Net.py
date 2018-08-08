ccb890c69cde412a889556e55fd13c13c1532cd8
antim <antimodebiase@hotmail.it>
2018-08-08 22:14:49 +0200

    Inserito il file Learning.py in Net.py

--- Net.py
+++ Net.py
@@ -2,7 +2,7 @@ import numpy as np
 from sympy import sympify
 from sympy import var, diff
 from sympy.utilities.lambdify import lambdify
-
+import copy as cp
 
 # mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
@@ -34,6 +34,72 @@ class Net:
                 print(self.activations[i + 1](3))
                 print(self.primes[i + 1](3))
 
+    # learning online
+    def train_net_online(self, training_set, validation_set, max_epoch, eta, error_function, alpha):
+        for t in range(max_epoch):
+            # permutazione del training set
+            np.random.shuffle(training_set)
+            for n in range(len(training_set)):
+                act_and_out = self.forward_propagation_with_output_hidden(training_set[n]['input'])
+                deltas = self.back_propagation(training_set[n]['label'], act_and_out['outputs'], act_and_out['activations'])
+                derivatives = self.calc_derivatives(training_set[n]['input'], act_and_out['outputs'], deltas)
+                self.update_weights(derivatives, eta)
+            # Calcolo dell'errore sul training set
+            error_training = 0
+            for n in range(len(training_set)):
+                output = self.forward_propagation(training_set[n]['input'])
+                error_training += error_function(output, training_set[n]['label'])
+            # Calcolo dell'errore sul validation set
+            error_validation = 0
+            for n in range(len(validation_set)):
+                output = self.forward_propagation(validation_set[n]['input'])
+                error_validation += error_function(output, validation_set[n]['label'])
+            # La rete di questo passo è migliore?
+            try:
+                if best_error_validation > error_validation:
+                    best_error_validation = error_validation
+                    best_W = cp.copy(self.W)
+                    best_B = cp.copy(self.B)
+            except NameError:
+                best_error_validation = error_validation
+                best_W = cp.copy(self.W)
+                best_B = cp.copy(self.B)
+            # Volendo si può applicare il criterio di fermata
+            glt = 100 * (error_validation / best_error_validation - 1)
+            if glt > alpha:
+                break
+        try:
+            self.W = best_W
+            self.B = best_B
+        except:
+            None
+
+    def back_propagation(self, labels, outputs, node_act):
+        # Calcolo delta
+        deltas = np.ndarray(self.n_layers, dtype=np.ndarray)
+        deltas[self.n_layers - 1] = self.primes[self.n_layers - 1](node_act[self.n_layers - 1]) * (outputs[self.n_layers - 1] - labels)
+        for l in range(self.n_layers - 2, -1, -1):
+            deltas[l] = np.dot(deltas[l + 1], self.W[l + 1])
+            deltas[l] = self.primes[l](node_act[l]) * deltas[l]
+        return deltas
+
+    def calc_derivatives(self, input, outputs, deltas):
+        # Calcolo derivate
+        derivate_W = []
+        derivate_B = []
+        Z = input
+        for l in range(self.n_layers):
+            derivate_W.append(np.dot(deltas[l][:, np.newaxis], Z[np.newaxis, :]))
+            derivate_B.append(deltas[l])
+            Z = outputs[l]
+        derivatives = {'weights': derivate_W, 'bias': derivate_B}
+        return derivatives
+
+    def update_weights(self, derivatives, eta):
+        # Aggiornamento dei pesi
+        for l in range(self.n_layers - 1):
+            self.W[l] = self.W[l] - eta * derivatives['weights'][l]
+            self.B[l] = self.B[l] - eta * derivatives['bias'][l]
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))  # activation function
