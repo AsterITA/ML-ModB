2a7a3d0b3acc869be7f9f4463b5a723d7ae60ae7
antim <antimodebiase@hotmail.it>
2018-08-10 14:17:58 +0200

    Backprop, learning online, batch funzionanti.

--- Net.py
+++ Net.py
@@ -45,26 +45,39 @@ class Net:
         return z, a
 
     # learning online
-    def train_net_online(self, training_set, validation_set, max_epoch, eta, error_function, alpha):
+    def train_net(self, training_set, validation_set, max_epoch, eta, error_function, alpha, online_flag):
         for t in range(max_epoch):
-            # permutazione del training set
-            np.random.shuffle(training_set)
+            if(online_flag):
+                # permutazione del training set
+                np.random.shuffle(training_set)
             for n in range(len(training_set)):
-                act_and_out = self.forward_propagation_with_output_hidden(training_set[n]['input'])
-                deltas = self.back_propagation(training_set[n]['label'], act_and_out['outputs'],
-                                               act_and_out['activations'])
-                derivatives = self.calc_derivatives(training_set[n]['input'], act_and_out['outputs'], deltas)
-                self.update_weights(derivatives, eta)
+                act_and_out = self.feed_forward(training_set[n]['input'])
+                derivatives = self.back_propagation(training_set[n]['input'], training_set[n]['label'], act_and_out[1],act_and_out[0])
+                if(online_flag):
+                    self.update_weights(derivatives, eta)
+                else:
+                    try:
+                        derivatives_tot['weights'] += derivatives['weights']
+                        derivatives_tot['bias'] += derivatives['bias']
+
+                    except NameError:
+                        derivatives_tot = derivatives
+            if(not online_flag):
+                self.update_weights(derivatives_tot, eta)
             # Calcolo dell'errore sul training set
-            error_training = 0
             for n in range(len(training_set)):
-                output = self.forward_propagation(training_set[n]['input'])
-                error_training += error_function(output, training_set[n]['label'])
+                act_and_out = self.feed_forward(training_set[n]['input'])
+                try:
+                    error_training += error_function(act_and_out[1][self.n_layers], training_set[n]['label'])
+                except NameError:
+                    error_training = error_function(act_and_out[1][self.n_layers], training_set[n]['label'])
             # Calcolo dell'errore sul validation set
-            error_validation = 0
             for n in range(len(validation_set)):
-                output = self.forward_propagation(validation_set[n]['input'])
-                error_validation += error_function(output, validation_set[n]['label'])
+                act_and_out = self.feed_forward(validation_set[n]['input'])
+                try:
+                    error_validation += error_function(act_and_out[1][self.n_layers], validation_set[n]['label'])
+                except NameError:
+                    error_validation = error_function(act_and_out[1][self.n_layers], validation_set[n]['label'])
             # La rete di questo passo è migliore?
             try:
                 if best_error_validation > error_validation:
@@ -72,35 +85,39 @@ class Net:
                     best_W = cp.copy(self.W)
                     best_B = cp.copy(self.B)
             except NameError:
-                best_error_validation = error_validation
-                best_W = cp.copy(self.W)
-                best_B = cp.copy(self.B)
+                try:
+                    best_error_validation = error_validation
+                    best_W = cp.copy(self.W)
+                    best_B = cp.copy(self.B)
+                except NameError:
+                    None
             # Volendo si può applicare il criterio di fermata
-            glt = 100 * (error_validation / best_error_validation - 1)
-            if glt > alpha:
-                break
+            try:
+                glt = 100 * (error_validation / best_error_validation - 1)
+                if glt > alpha:
+                    break
+            except NameError:
+                None
         try:
             self.W = best_W
             self.B = best_B
         except:
             None
 
-    def back_propagation(self, labels, outputs, node_act):
+    def back_propagation(self, input, labels, outputs, node_act):
         # Calcolo delta
-        deltas = np.ndarray(self.n_layers, dtype=np.ndarray)
-        deltas[self.n_layers - 1] = self.primes[self.n_layers - 1](node_act[self.n_layers - 1]) * (
-                outputs[self.n_layers - 1] - labels)
-        for l in range(self.n_layers - 2, -1, -1):
-            deltas[l] = np.dot(deltas[l + 1], self.W[l + 1])
-            deltas[l] = self.primes[l](node_act[l]) * deltas[l]
-        return deltas
-
-    def calc_derivatives(self, input, outputs, deltas):
+        #Probabilmente il calcolo del delta va virtualizzato.
+        #Il seguente è valido se si utilizza la somma dei quadrati e la cross entropy
+        deltas  = {}
+        deltas[self.n_layers] = self.primes[self.n_layers-1](node_act[self.n_layers]) * (outputs[self.n_layers] - labels)
+        for l in range(self.n_layers - 1, 1, -1):
+            deltas[l] = np.dot(deltas[l + 1], self.W[l].transpose())
+            deltas[l] = self.primes[l-1](node_act[l]) * deltas[l]
         # Calcolo derivate
         derivate_W = []
         derivate_B = []
         Z = input
-        for l in range(self.n_layers):
+        for l in range(2, self.n_layers + 1):
             derivate_W.append(np.dot(deltas[l][:, np.newaxis], Z[np.newaxis, :]))
             derivate_B.append(deltas[l])
             Z = outputs[l]
@@ -108,10 +125,10 @@ class Net:
         return derivatives
 
     def update_weights(self, derivatives, eta):
-        # Aggiornamento dei pesi
-        for l in range(self.n_layers - 1):
-            self.W[l] = self.W[l] - eta * derivatives['weights'][l]
-            self.B[l] = self.B[l] - eta * derivatives['bias'][l]
+        # Aggiornamento dei pesi Metodo discesa del gradiente
+        for l in range(1, self.n_layers):
+            self.W[l] = self.W[l] - eta * derivatives['weights'][l-1].transpose()
+            self.B[l] = self.B[l] - eta * derivatives['bias'][l-1]
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))  # activation function
@@ -142,3 +159,24 @@ f = lambdify(x, expr)  # Con questo si trasforma l'input in una funzione
 functions[2] = f
 
 NN=Net([2, 3, 1], functions)
+
+#  IL SEGUENTE E' IL CODICE CHE HO USATO PER TESTARE IL LEARNING ONLINE E IL BATCH
+#genero un training set
+training_set = []
+for i in range(10):
+    inpu = np.random.randint(0,4,4)
+    app = np.random.randint(0,2,1)
+    out = np.zeros(3)
+    out[app] = 1
+    elem = {'input':inpu, 'label': out}
+    training_set.append(elem)
+#print(training_set)
+functions[1] = sigmoid
+functions[2] = sigmoid
+mia_net = Net([4, 4, 3], functions)
+print("W= ", mia_net.W)
+print("B= ", mia_net.B)
+error_function = sum_square
+mia_net.train_net(training_set,[],50,0.5,error_function,10, False) #False = BATCH; True = ONLINE
+print("W= ", mia_net.W)
+print("B= ", mia_net.B)
